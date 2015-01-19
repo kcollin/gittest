11db311f25a9a4e1f4a0de3a94e5966868c5f000
Knut Collin <kc@hin.no>
2015-01-19 12:43:47 +0100

    Initial commit

---
+++ GitTest1/src/DemoGit.java
@@ -0,0 +1,30 @@
+
+public class DemoGit {
+	private int count;
+	private String name;
+	private int[] numbers;
+	
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int[] getNumbers() {
+		return numbers;
+	}
+	public void setNumbers(int[] numbers) {
+		this.numbers = numbers;
+	}
+
+	public void test() {
+		
+		System.out.println("Test 1");
+	}
+}
