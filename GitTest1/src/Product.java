d275cff9992ccacc7da854791831d64b340f6dda
Knut Collin <kc@hin.no>
2015-01-19 15:15:38 +0100

    Method test2 added in product class

---
+++ GitTest1/src/Product.java
@@ -0,0 +1,17 @@
+
+public class Product {
+
+	private int count;
+	
+	
+	private void test1() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void test2() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
