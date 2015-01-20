b80caef175fa732e0900e57ef7153afa4a08eef7
Knut Collin <kc@hin.no>
2015-01-20 09:09:19 +0100

    New class Train added

---
+++ GitTest1/src/Train.java
@@ -0,0 +1,29 @@
+
+
+public class Train {
+
+	private int serial;
+	private String name;
+	private String model;
+	
+	public int getSerial() {
+		return serial;
+	}
+	public void setSerial(int serial) {
+		this.serial = serial;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+}
+
+
