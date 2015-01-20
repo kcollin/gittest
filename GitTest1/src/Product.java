4d4efd30c1df509ae2588720d8c51fb1f13a9185
Knut Collin <kc@hin.no>
2015-01-20 08:22:06 +0100

    New class product 20/1

---
+++ GitTest1/src/Product.java
@@ -0,0 +1,19 @@
+
+
+public class Product {
+
+	private int count;
+	
+	
+	private void test1() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void test2() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
